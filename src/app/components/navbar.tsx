2e47c2ee45efdf1e51e119e84a37e87096e99a0a
root_rooster <gmail@nikcadez.com>
2024-08-28 22:15:27 +0200

    nav bar quick fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -14,14 +14,16 @@ export default function NavBar() {
       <header className="text-white font-bold">
         <nav className="flex justify-between items-center w-[92%] mx-auto">
           <div className="pl-5 py-8">
-            <Logo color="white" />
+            <Link href="/">
+              <Logo color="white" />
+            </Link>
           </div>
           <div
-            className={`md:static z-50 md:min-h-fit absolute min-h-[100vh] left-0 ${isExpanded ? "top-0 bg-blue-700" : "top-[-100%]"} md:w-auto w-full flex items-center px-5`}
+            className={`static z-50 min-h-fit left-0 md:w-auto w-full flex items-center px-5`}
           >
-            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-20  w-full justify-center items-center">
+            <ul className="flex flex-row items-center gap-[4vw] w-full justify-center">
               <li>
-                <Link onClick={handleToggle} className="hover:text-gray-300" href="/">
+                <Link onClick={handleToggle} className="hover:text-gray-300 hidden md:block" href="/">
                   DOMOV
                 </Link >
               </li>
@@ -36,32 +38,13 @@ export default function NavBar() {
                 </Link >
               </li>
               <li>
-                <Link onClick={handleToggle} className="hover:text-gray-300" href="/team">
+                <Link onClick={handleToggle} className="hover:text-gray-300 hidden md:block" href="/team">
                   O NAS
                 </Link >
               </li>
             </ul>
           </div>
 
-          <div
-            onClick={handleToggle}
-            className="flex items-center gap-6 md:hidden click:bg-gray-100 hover:bg-best-blue-900/30 border p-2 rounded-lg border-gray-300"
-          >
-            <svg
-              xmlns="http://www.w2.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </div>
         </nav>
       </header>
     </>
