f81f8a1c337f54f41d4b8b492602836f92d4fc6f
root_rooster <gmail@nikcadez.com>
2024-08-29 22:43:12 +0200

    new title

--- src/app/publication/page.tsx
+++ src/app/publication/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <>
       <Hero
-        title="Oddaj svoj Poster minute"
+        title="Poster minute"
         text="V okviru dogodka BEST dnevi znanosti bo v popoldanskem delu konference v torek, 22. oktobra 2024, potekala poster sekcija »POSTER MINUTE«, kjer lahko študenti prek posterjev predstavite svoje diplomsko, magistrsko, doktorsko ali drugo raziskovalno delo, ki je povezano s kemijo, biokemijo in/ali z računalništvom."
         buttonText="Oddaj poster"
       />
