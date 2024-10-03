59edb25aaeca2a0776656c2fcaa21882b85f756e
root_rooster <gmail@nikcadez.com>
2024-10-03 21:01:12 +0200

    added margin

--- src/app/publication/page.tsx
+++ src/app/publication/page.tsx
@@ -18,40 +18,40 @@ export default function Page() {
             Oddaj svoj POSTER - Poster minute 2024
           </h1>
 
-          <div className="pt-6 pb-12">
+          <div className="pt-6 pb-7">
             <p>
               V torek, 22. oktobra, bo v sklopu dogodka BEST dnevi znanosti
               potekala predstavitev na plakatih ali Poster minute.
             </p>
 
-            <p className="mt-12">
+            <p className="mt-7">
               Na plakatu lahko predstaviš svoje diplomsko, magistrsko, doktorsko
               ali drugo raziskovalno delo.
             </p>
 
-            <p className="mt-12">
+            <p className="mt-7">
               Plakat bo razstavljen na dogodku in ga lahko prideš tudi
               predstaviti. Objavljen pa bo tudi na spletni strani dogodka BEST
               dnevi znanosti.
             </p>
 
-            <p className="mt-12">
+            <p className="mt-7">
               Oddaja je odprta za študente vseh fakultet, katerih raziskovalna
               dela se navezujejo na vsaj eno izmed sledečih področij: kemija,
               biokemija, računalništvo in tehnologija.
             </p>
 
-            <p className="mt-12">
+            <p className="mt-7">
               Oddaja je odprta do petka, 4. oktobra, oddaje pa zbiramo preko
               spodnjega obrazca, število mest je omejeno.
             </p>
 
-            <p className="mt-12">
+            <p className="mt-7">
               V primeru vprašanj nam piši na: bdz@bestljubljana.si
             </p>
           </div>
           <div
-            className={`flex columns-2 duration-300 justify-start mt-12 md:mt-20`}
+            className={`flex mb-20 columns-2 duration-300 justify-start mt-12 md:mt-20`}
           >
             <a
               className={`transition ease-in-out delay-150 hover:scale-110 hover:-translate-y-1 bg-best-blue-100 shadow-md hover:shadow-xl font-bold p-5 md:px-20 text-xl md:text-base px-32 rounded-full text-best-blue-900`}
