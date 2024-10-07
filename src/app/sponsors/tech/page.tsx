18a35b97bad226385f6f6b64e42326ed489821a0
root_rooster <gmail@nikcadez.com>
2024-10-07 12:30:38 +0200

    proton description

--- src/app/sponsors/tech/page.tsx
+++ src/app/sponsors/tech/page.tsx
@@ -5,7 +5,80 @@ export default function Page() {
       <CompanyP>
         <div>
           <h1 className="mt-12 text-4xl">Proton PharmaTech</h1>
+          <p>ENG:</p>
+          <p className="">
+            Founded in 2022, Porton PharmaTech d.o.o., Slovenia, is the European
+            subsidiary of Porton Pharma Solutions Ltd. It is the first CDMO in
+            the pharmaceutical industry in Slovenia, focused on research,
+            development and manufacturing of API’s. We are dedicated to
+            providing comprehensive services to Customers, encompassing
+            synthesis route development, process familiarization and
+            optimization, analytical methods development, validation and process
+            scale-up. At Porton PharmaTech, d.o.o. customers are fully supported
+            by an integrated project team committed to deliver services and
+            products within expected timelines and quality.
+          </p>
           <p className="mt-7">
+            Porton PharmaTech is currently undergoing development, with new
+            constructions and facility enhancements underway. In 2024 we have
+            started with the R&D work in our state-of-the-art equipped
+            laboratories. We are finalizing the kilo-lab facility, as well as
+            the investment in the Pilot plant designed to operate with five
+            equipment trains and finishing area, QC laboratories, additional R&D
+            laboratories and Process Safety Laboratory.
+          </p>
+
+          <p className="my-7">
+            We are knowledgeable, passionate, and united by our vision to enable
+            the public&apos;s early access to good medicines. We are much more
+            than just a CDMO company in pharmaceutical business, as we can offer
+            a chance to upgrade not just your expert knowledge and test yourself
+            by being actively involved in company growth, but also learn various
+            business skills in an international, ambitious, and dynamic
+            organization.
+          </p>
+          <p>SLO:</p>
+          <p className="">
+            Ustanovljen leta 2022, Porton PharmaTech d.o.o., Slovenija je
+            evropska podružnica Porton Pharma Solutions Ltd. Je prvo podjetje v
+            farmacevtski industriji v Sloveniji za pogodbene raziskave in razvoj
+            ter proizvodnjo (angl. CDMO=Contract Development and Manufacturing
+            Organization), osredotočeno na razvoj in proizvodnjo farmacevtskih
+            učinkovin. Smo predani zagotavljanju celovitih storitev za stranke,
+            kar zajema razvoj sinteznih poti, seznanjanje s procesi in
+            optimizacije procesov, razvoj analitičnih metod, validacije in
+            nadgradnje procesov. V podjetju Porton PharmaTech, d.o.o. stranke v
+            celoti podpira integrirana projektna skupina, ki je zavezana
+            zagotavljanju storitev in izdelkov v pričakovanih rokih in
+            kakovosti.
+          </p>
+          <p className="mt-7">
+            Imamo veliko znanja, smo predani našemu delu in sledimo viziji, da
+            javnosti omogočimo zgodnji dostop do dobrih zdravil. Smo veliko več
+            kot le storitveno podjetje v farmacevtski industriji, saj lahko
+            nudimo možnosti tako za nadgradnjo strokovnega znanja in
+            preizkušanje sposobnosti posameznikov skozi aktivno vključevanje v
+            rast podjetja kot tudi pridobivanje različnih poslovnih veščin v
+            mednarodni, ambiciozni in dinamični organizaciji.
+          </p>
+          <p className="mt-7">
+            Porton PharmaTech je trenutno v fazi rasti, ki jo spremljajo
+            investicije v obstoječe zmogljivosti ter investicijske gradnje novih
+            objektov. Leta 2024 smo pričeli z delom v naših najsodobneje
+            opremljenih raziskovalno- razvojnih laboratorijih. V zaključni fazi
+            je kilo-laboratorij ter naložba v pilotni obrats petimi linijami za
+            kemijske operacije s finalizacijo, laboratorijem za kontrolo
+            kakovosti, dodatnimi raziskovalno-razvojnimi laboratorijiin
+            laboratorijem za procesno varnost.
+          </p>
+          <p className="mt-7">
+            Imamo veliko znanja, smo predani našemu delu in sledimo viziji, da
+            javnosti omogočimo zgodnji dostop do dobrih zdravil. Smo veliko več
+            kot le storitveno podjetje v farmacevtski industriji, saj lahko
+            nudimo možnosti tako za nadgradnjo strokovnega znanja in
+            preizkušanje sposobnosti posameznikov skozi aktivno vključevanje v
+            rast podjetja kot tudi pridobivanje različnih poslovnih veščin v
+            mednarodni, ambiciozni in dinamični organizaciji.
           </p>
         </div>
       </CompanyP>
